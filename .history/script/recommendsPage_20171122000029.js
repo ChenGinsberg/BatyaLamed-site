812887083cc8e09186b4f12ca909f6ceea9e70de
chen ginsberg <chenogin@gmail.com>
2017-11-22 00:19:29 +0200

    fix nav and logo in each page

--- .history/script/recommendsPage_20171122000029.js
+++ .history/script/recommendsPage_20171122000029.js
@@ -2,6 +2,6 @@ $(document).ready(function () {
     $("img").click(function () {
         var t = $(this).attr("src");
         $(".modal-body").html("<img src='" + t + "' class='modal-img'>");
-        $("#myModal").Modal();
+        $("#myModal").click();
     });
 });
--- .history/script/recommendsPage_20171122000103.js
+++ .history/script/recommendsPage_20171122000103.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     $("img").click(function () {
         var t = $(this).attr("src");
-        $(".modal-body").html("<img src='" + t + "' class='modal-img'>");
-        $("#myModal").Modal();
+        $(".body").html("<img src='" + t + "' class='modal-img'>");
+        $("#myModal").();
     });
 });
